7b17d04c692deddec9d40a6a3cd860bd091e088b
Alex <alex02605@gmail.com>
2021-12-19 11:46:20 +0200

    vv

---
+++ allo.js
@@ -0,0 +1,6 @@
+let elem = document.getElementById("allo")
+
+elem.addEventListener("click", function(){
+    alert(Math.floor(Math.random()*56 )+ Math.floor(Math.random()*24 ) )
+    
+})                                                          
